7ef4bdb720a839869076ee512ecfa7eea704b809
Yoga Prawira Kusuma <96771743+Yogaprawira852@users.noreply.github.com>
2022-06-09 22:39:56 +0700

    Update artikel.sql

--- TA/artikel.sql
+++ TA/artikel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2021 at 12:48 PM
+-- Generation Time: Jun 09, 2022 at 12:48 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -41,7 +41,7 @@ CREATE TABLE `t_artikel` (
 INSERT INTO `t_artikel` (`id`, `judul`, `isi`, `gambar`) 
 --
 -- Indexes for dumped tables
---
+-- 
 
 --
 -- Indexes for table `t_artikel`
